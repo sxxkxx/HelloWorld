4815e4cc70feb68886a280bb2b55590edcb6aac9
sankim <sankim@localhost>
2023-07-16 20:46:51 +0900

    23/07/16

--- H20230714/src/co/yedam/ForAndFor.java
+++ H20230714/src/co/yedam/ForAndFor.java
@@ -3,30 +3,39 @@ package co.yedam;
 public class ForAndFor {
 
 	public static void main(String[] args) {
-		practice3();
-	} //end of main
-	
+		practice4();
+	} // end of main
+
 	public static void practice3() {
 		boolean run = true;
-		
-		while(run) {
-//			int n1 = (int)(Math.random()* 6 + 1);
-//			int n2 = (int)(Math.random()* 6 + 1);
-//			if(n1+n2 == 5) {
-//				System.out.printf("(%d, %d)",n1,n2);
-//				run=false;
-//			}
-//			
-//		}
-		
+		int n1 = 0;
+		int n2 = 0;
+
+		while (run) {
+			n1 = (int) (Math.random() * 6 + 1);
+			n2 = (int) (Math.random() * 6 + 1);
+			if (n1 + n2 == 5) {
+				run = false;
+			}
+
+		}
+		System.out.printf("(%d, %d)", n1, n2);
+
 	}// end of practice3
-	
+
 	public static void practice4() {
-		
+		// 4x + 5y ==60;
+		for (int x = 1; x <= 10; x++) {
+			for (int y = 1; y <= 10; y++) {
+				if (4 * x + 5 * y == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+		}
 	}// end of practice4
-	
+
 	public static void forAndfor() {
-		for (int j=1;j<=9;j++) {
+		for (int j = 1; j <= 9; j++) {
 			for (int i = 1; i <= 9; i++) {
 				System.out.printf("%d * %d = %d\n", j, i, j * i);
 			}
